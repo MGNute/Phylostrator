30a07302d3ab4e018f65a58fd1779b274dfd478d
MGNute <mike.nute@gmail.com>
2016-09-28 22:42:57 -0400

    removing the my gloabls

--- my_globals.py
+++
@@ -1,5 +0,0 @@
-
-jitter_radius=3
-amato_qiime_tree = '/Users/michaelnute/Dropbox/Grad School/Phylogenetics/work/kra-primate-project/kra-primate/tree_placement/rdp_tree_leaves_renamed.tre'
-amato_qiime_root = '/Users/michaelnute/Dropbox/Grad School/Phylogenetics/work/kra-primate-project/kra-primate/tree_placement/'
-amato_temp_subtree = '/Users/michaelnute/Dropbox/Grad School/Phylogenetics/work/kra-primate-project/kra-primate/tree_placement/rdp_temp_subtree_mac.tre'
